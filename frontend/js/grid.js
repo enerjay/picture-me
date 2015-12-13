c028f7cfc7364e4c11a8d957978a74e84e126c2f
enerjay <enerjay@gmail.com>
2015-12-13 20:36:34 +0000

    fixing the grid for mobile

--- frontend/js/grid.js
+++ frontend/js/grid.js
@@ -2,8 +2,7 @@ $(document).ready( function() {
 
   $('.grid').masonry({
     itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition:true
+    columnWidth: 50
   });
   
 });
